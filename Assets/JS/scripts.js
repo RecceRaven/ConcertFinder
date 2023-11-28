7562fae7211b751d8d05fdd6730bdac5a0c69b89
jwatsoncodes <149508637+jwatsoncodes@users.noreply.github.com>
2023-11-28 09:12:41 -0500

    added interactive mapbox, needs additional formatting

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -21,14 +21,5 @@ $(document).ready(function() {
     };
 });
 
-	// TO MAKE THE MAP APPEAR YOU MUST
-	// ADD YOUR ACCESS TOKEN FROM
-	// https://account.mapbox.com
-	//mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-//const map = new mapboxgl.Map({
-//container: 'map', // container ID
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-//style: 'mapbox://styles/mapbox/streets-v12', // style URL
-//center: [-74.5, 40], // starting position [lng, lat]
-//zoom: 9 // starting zoom
+
 
